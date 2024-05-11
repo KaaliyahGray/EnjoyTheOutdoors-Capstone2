780422c1660ad075fda9f9815bbc1e808a71c09c
KaaliyahGray <166437615+KaaliyahGray@users.noreply.github.com>
2024-05-10 23:42:08 -0700

    created java for dropdown
    created java for dropdown

--- scripts.js/mountains.js
+++ scripts.js/mountains.js
@@ -1,15 +1,507 @@
 "use strict";
 
- // Array of student names
- const students = ["Alice", "Bob", "Charlie", "David", "Eva"];
+ // Array of mountains
+ let mountains = [
+    {
+      Mountname: "first",
+      description: "",
+      elevation: "",
+      location: "",
+      imageurl: "",
+      levelofdifficulty: ""
+    
+    },
+    {
+        Mountname: "second",
+        description: "",
+        elevation: "",
+        location: "",
+        imageurl: "",
+        levelofdifficulty: ""
+     
+    },
+    {
+        Mountname: "",
+        description: "",
+        elevation: "",
+        location: "",
+        imageurl: "",
+        levelofdifficulty: ""
+     
+    },
+    {
+        Mountname: "",
+        description: "",
+        elevation: "",
+        location: "",
+        imageurl: "",
+        levelofdifficulty: ""
+     
+    },
+    {
+        Mountname: "",
+        description: "",
+        elevation: "",
+        location: "",
+        imageurl: "",
+        levelofdifficulty: ""
+     
+    },
+    {
+        Mountname: "",
+        description: "",
+        elevation: "",
+        location: "",
+        imageurl: "",
+        levelofdifficulty: ""
+     
+    },
+    {
+        Mountname: "",
+        description: "",
+        elevation: "",
+        location: "",
+        imageurl: "",
+        levelofdifficulty: ""
+     
+    },
+    {
+        Mountname: "",
+        description: "",
+        elevation: "",
+        location: "",
+        imageurl: "",
+        levelofdifficulty: ""
+     
+    },
+    {
+        Mountname: "",
+        description: "",
+        elevation: "",
+        location: "",
+        imageurl: "",
+        levelofdifficulty: ""
+     
+    },
+    {
+        Mountname: "",
+        description: "",
+        elevation: "",
+        location: "",
+        imageurl: "",
+        levelofdifficulty: ""
+     
+    },
+    {
+        Mountname: "",
+        description: "",
+        elevation: "",
+        location: "",
+        imageurl: "",
+        levelofdifficulty: ""
+     
+    },
+    {
+        Mountname: "",
+        description: "",
+        elevation: "",
+        location: "",
+        imageurl: "",
+        levelofdifficulty: ""
+     
+    },
+    {
+        Mountname: "",
+        description: "",
+        elevation: "",
+        location: "",
+        imageurl: "",
+        levelofdifficulty: ""
+     
+    },
+    {
+        Mountname: "",
+        description: "",
+        elevation: "",
+        location: "",
+        imageurl: "",
+        levelofdifficulty: ""
+     
+    },
+    {
+        Mountname: "",
+        description: "",
+        elevation: "",
+        location: "",
+        imageurl: "",
+        levelofdifficulty: ""
+     
+    },
+    {
+        Mountname: "",
+        description: "",
+        elevation: "",
+        location: "",
+        imageurl: "",
+        levelofdifficulty: ""
+     
+    },
+    {
+        Mountname: "",
+        description: "",
+        elevation: "",
+        location: "",
+        imageurl: "",
+        levelofdifficulty: ""
+     
+    },
+    {
+        Mountname: "",
+        description: "",
+        elevation: "",
+        location: "",
+        imageurl: "",
+        levelofdifficulty: ""
+     
+    },
+    {
+        Mountname: "",
+        description: "",
+        elevation: "",
+        location: "",
+        imageurl: "",
+        levelofdifficulty: ""
+     
+    },
+    {
+        Mountname: "",
+        description: "",
+        elevation: "",
+        location: "",
+        imageurl: "",
+        levelofdifficulty: ""
+     
+    },
+    {
+        Mountname: "",
+        description: "",
+        elevation: "",
+        location: "",
+        imageurl: "",
+        imageurl: "",
+        levelofdifficulty: ""
+      
+      },
+      {
+          Mountname: "",
+          description: "",
+          elevation: "",
+          location: "",
+          imageurl: "",
+          levelofdifficulty: ""
+       
+      },
+      {
+          Mountname: "",
+          description: "",
+          elevation: "",
+          location: "",
+          imageurl: "",
+          levelofdifficulty: ""
+       
+      },
+      {
+          Mountname: "",
+          description: "",
+          elevation: "",
+          location: "",
+          imageurl: "",
+          levelofdifficulty: ""
+       
+      },
+      {
+          Mountname: "",
+          description: "",
+          elevation: "",
+          location: "",
+          imageurl: "",
+          levelofdifficulty: ""
+       
+      },
+      {
+          Mountname: "",
+          description: "",
+          elevation: "",
+          location: "",
+          imageurl: "",
+          levelofdifficulty: ""
+       
+      },
+      {
+          Mountname: "",
+          description: "",
+          elevation: "",
+          location: "",
+          imageurl: "",
+          levelofdifficulty: ""
+       
+      },
+      {
+          Mountname: "",
+          description: "",
+          elevation: "",
+          location: "",
+          imageurl: "",
+          levelofdifficulty: ""
+       
+      },
+      {
+          Mountname: "",
+          description: "",
+          elevation: "",
+          location: "",
+          imageurl: "",
+          levelofdifficulty: ""
+       
+      },
+      {
+          Mountname: "",
+          description: "",
+          elevation: "",
+          location: "",
+          imageurl: "",
+          levelofdifficulty: ""
+       
+      },
+      {
+          Mountname: "",
+          description: "",
+          elevation: "",
+          location: "",
+          imageurl: "",
+          levelofdifficulty: ""
+       
+      },
+      {
+          Mountname: "",
+          description: "",
+          elevation: "",
+          location: "",
+          imageurl: "",
+          levelofdifficulty: ""
+       
+      },
+      {
+          Mountname: "",
+          description: "",
+          elevation: "",
+          location: "",
+          imageurl: "",
+          levelofdifficulty: ""
+       
+      },
+      {
+          Mountname: "",
+          description: "",
+          elevation: "",
+          location: "",
+          imageurl: "",
+          levelofdifficulty: ""
+       
+      },
+      {
+          Mountname: "",
+          description: "",
+          elevation: "",
+          location: "",
+          imageurl: "",
+          levelofdifficulty: ""
+       
+      },
+      {
+          Mountname: "",
+          description: "",
+          elevation: "",
+          location: "",
+          imageurl: "",
+          levelofdifficulty: ""
+       
+      },
+      {
+          Mountname: "",
+          description: "",
+          elevation: "",
+          location: "",
+          imageurl: "",
+          levelofdifficulty: ""
+       
+      },
+      {
+          Mountname: "",
+          description: "",
+          elevation: "",
+          location: "",
+          imageurl: "",
+          levelofdifficulty: ""
+       
+      },
+      {
+          Mountname: "",
+          description: "",
+          elevation: "",
+          location: "",
+          imageurl: "",
+          levelofdifficulty: ""
+       
+      },
+      {
+          Mountname: "",
+          description: "",
+          elevation: "",
+          location: "",
+          imageurl: "",
+          levelofdifficulty: ""
+       
+      },
+      {
+        Mountname: "",
+        description: "",
+        elevation: "",
+        location: "",
+        imageurl: "",
+        levelofdifficulty: ""
+     
+    },  {
+        Mountname: "",
+        description: "",
+        elevation: "",
+        location: "",
+        imageurl: "",
+        levelofdifficulty: ""
+     
+    },  {
+        Mountname: "",
+        description: "",
+        elevation: "",
+        location: "",
+        imageurl: "",
+        levelofdifficulty: ""
+     
+    },  {
+        Mountname: "",
+        description: "",
+        elevation: "",
+        location: "",
+        imageurl: "",
+        levelofdifficulty: ""
+     
+    },  {
+        Mountname: "",
+        description: "",
+        elevation: "",
+        location: "",
+        imageurl: "",
+        levelofdifficulty: ""
+     
+    },  {
+        Mountname: "",
+        description: "",
+        elevation: "",
+        location: "",
+        imageurl: "",
+        levelofdifficulty: ""
+     
+    },  {
+        Mountname: "",
+        description: "",
+        elevation: "",
+        location: "",
+        imageurl: "",
+        levelofdifficulty: ""
+     
+    },  {
+        Mountname: "",
+        description: "",
+        elevation: "",
+        location: "",
+        imageurl: "",
+        levelofdifficulty: ""
+     
+    }];
+ // Get the dropdown element and display the info
+ const dropdown = document.getElementById('mountainDropdown');
+ const infoDisplay = document.getElementById("mountainInfo");
 
- // Get the dropdown element
- const dropdown = document.getElementById('studentDropdown');
- // Populate the dropdown using a for loop
- for (let index = 0; index < students.length; index++) {
-     const option = document.createElement('option');  // Create a new <option> element
-     option.value = students[index];  // Set the value attribute of the option
-     option.textContent = students[index];  // Set the text content of the option
-     dropdown.appendChild(option);  // Add the option to the dropdown
- }
+ // Populate the dropdown options
+mountains.forEach(function(mountain) {
+    let option = document.createElement("option");
+    option.text = mountain.Mountname;
+    option.value = mountain.Mountname; // You can use a unique identifier here if needed
+    dropdown.add(option);
+});
+// Add event listener to the dropdown to detect changes
+dropdown.addEventListener("change", function() {
+    // Retrieve the selected option
+    const selectedMountainName = dropdown.value;
 
+    // Find the selected mountain object from the mountains array
+    const selectedMountain = mountains.find(mountain => mountain.Mountname === selectedMountainName);
+
+    // Display information based on the selected mountain object
+    if (selectedMountain) {
+        infoDisplay.innerHTML = `
+            <h2>${selectedMountain.Mountname}</h2>
+            <p><strong>Description:</strong> ${selectedMountain.description}</p>
+            <p><strong>Elevation:</strong> ${selectedMountain.elevation}</p>
+            <p><strong>Location:</strong> ${selectedMountain.location}</p>
+            <p><strong>Level of Difficulty:</strong> ${selectedMountain.levelofdifficulty}</p>
+            <img src="${selectedMountain.imageurl}" alt="${selectedMountain.Mountname}">
+        `;
+    } else {
+        infoDisplay.textContent = ""; // Clear the information display if no option is selected
+    }
+});
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//   // Populate the dropdown using a for loop
+//   for (let index = 0; index < mountains.length; index++) {
+//     const option = document.createElement('option');  // Create a new <option> element
+//     option.value = mountains[index];  // Set the value attribute of the option
+//     option.textContent = mountains[index];  // Set the text content of the option
+//     dropdown.appendChild(option);  // Add the option to the dropdown
+// }
--- scripts.js/parks.js
+++ scripts.js/parks.js
@@ -1,1 +1,46 @@
 "use strict";
+
+const nationalPark = [
+    {name: "this", parkType:"", location: 'Arizona' },
+    {name: "practice", parkType:"Gym", location: '' },
+    {name: "is", parkType:"", location: 'Arizona' },
+    {name: "for", parkType:"Gym", location: '' },
+    {name: "practice", parkType:"", location: 'Arizona' },
+    {name: "the type", parkType:"Gym", location: '' },
+    {name: "Good Job", parkType:"", location: 'Arizona' },
+
+
+
+];
+
+ // function with the loop
+function location(nationalPark) {
+    let i = 0;
+    while (i < nationalPark.length) {
+        const park = nationalPark[i];
+        if (park.location >= "Arizona") {
+            console.log(`Here are your choices of parks in AZ ${park.name}`);
+        } else {
+          
+        }
+        i++;
+    }
+}    
+// Call the function
+location(nationalPark);
+
+ // function with the loop
+ function Type(nationalPark) {
+    let i = 0;
+    while (i < nationalPark.length) {
+        const park = nationalPark[i];
+        if (park.parkType >= "Gym") {
+            console.log(`Here are your choices gym parks ${park.name}`);
+        } else {
+          
+        }
+        i++;
+    }
+}    
+// Call the function
+Type(nationalPark);
