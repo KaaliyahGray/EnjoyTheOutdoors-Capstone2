ee031f5ededc7f2ce4ebe1232615d870e9f56441
KaaliyahGray <166437615+KaaliyahGray@users.noreply.github.com>
2024-05-18 20:29:15 -0700

    updating java
    updating java

--- scripts.js/mountains.js
+++ scripts.js/mountains.js
@@ -3,202 +3,256 @@
  // Array of mountains
  let mountains = [
     {
-      Mountname: "first",
-      description: "",
-      elevation: "",
-      location: "hey",
-      imageurl: "",
-      levelofdifficulty: ""
+        imageurl: "https://upload.wikimedia.org/wikipedia/commons/2/2e/White_Mountains_12_30_09_81.jpg",
+    Mountname: "Mt Washington",
+      description: "Mt. Washington (6,288 feet) is the highest peak east of the Mississippi River and north of the Carolinas. The upper part of the mountain has a climate similar to that of northern Labrador and supports a variety of alpine flora and fauna.",
+      elevation: "6288",
+      coords: " Lat - 44.270403 Lng -71.303501",
+      levelofdifficulty: "Strenuous"
     
     },
     {
-        Mountname: "second",
-        description: "",
-        elevation: "",
-        location: "",
         imageurl: "",
-        levelofdifficulty: ""
+        Mountname: "Mt. Adams",
+        description: "Mt. Adams (5,799') is the second highest peak in New England, offering spectacular views across the Great Gulf and King Ravine.",
+        elevation: "5799",
+        coords:"  lat: 44.320686,-71.291742",
+        levelofdifficulty: "Strenuous"
      
     },
     {
-        Mountname: "third",
-        description: "",
-        elevation: "",
-        location: "",
         imageurl: "",
-        levelofdifficulty: ""
+        Mountname: "Mt Jefferson",
+        description: "Mt. Jefferson (5,716 feet) has three summits a short distance apart, in line northwest and southeast, with the highest in the middle. Perhaps the most striking view is down the Great Gulf with the Carter Range beyond.",
+        elevation: "5716",
+        coords: "",
+        levelofdifficulty: "Strenuous"
      
     },
     {
+        Mountname: "Mt Monroe",
+        description: "Mt. Monroe (5,372 feet) is the highest of the Southern Presidentials, and is a sharply pointed peak rising abruptly from the area around the Lakes of the Clouds. The summit is completely above treeline and affords fine views of the deep chasm of Oakes Gulf.",
+        elevation: "5372",
+        coords: "",
+        imageurl: "",
+        levelofdifficulty: "Strenuous"
+     
+    },
+    {
+        imageurl: "",
+        Mountname: "Mt Madison",
+        description: "Mt. Madison (5,366 feet) is the farthest northeast of the high peaks of the Presidential Range, remarkable for the great drop of more than 4,000 feet to the river valleys east and northeast from the summit.",
+        elevation: "5366",
+        coords: "",
+        levelofdifficulty: "Strenuous"
+     
+    },
+    {
+        imageurl: "",
+        Mountname: "Mt. Lafayette",
+        description: "Mt. Lafayette (5,260 feet) represents the highest peak in the Franconia Ridge and, from the summit, offers a stunning view of the Pemigewasset Wilderness Area.",
+        elevation: "5260",
+        coords: "",
+        levelofdifficulty: "Strenuous"
+     
+    },
+    {
+        imageurl: "",
+        Mountname: "Mt. Lincoln",
+        description: "Mt. Lincoln (5,089 feet), a part of the popular and scenic Franconia Ridge, bears a sharp, rugged peak with excellent views of the surrounding area and an array of alpine plants.",
+        elevation: "5089",
+        coords: "",
+        levelofdifficulty: "Strenuous"
+     
+    },
+    {
+        imageurl: "",
+        Mountname: "South Twin",
+        description: "South Twin Mountain (4,902 feet) offers sweeping views to the south of the Franconia Ridge and Franconia Brook valleys.",
+        elevation: "4902",
+        coords: "",
+        levelofdifficulty: "Strenuous"
+     
+    },
+    {
+        imageurl: "",
+        Mountname: "Carter Dome",
+        description: "Carter Dome (4,832'), in the Carter-Moriah Range, offers dramatic, close-up views of Mount Washington and the Presidential Range from its northeastern shoulder, Mount Hight.",
+        elevation: "4832",
+        coords: "",
+        levelofdifficulty: "Strenuous"
+     
+    },
+    {
+        imageurl: "",
         Mountname: "",
         description: "",
         elevation: "",
         location: "",
-        imageurl: "",
-        levelofdifficulty: ""
+        levelofdifficulty: "Strenuous"
      
     },
     {
+        imageurl: "",
         Mountname: "",
         description: "",
         elevation: "",
         location: "",
-        imageurl: "",
-        levelofdifficulty: ""
+        levelofdifficulty: "Strenuous"
      
     },
     {
+        imageurl: "",
         Mountname: "",
         description: "",
         elevation: "",
         location: "",
-        imageurl: "",
-        levelofdifficulty: ""
+        levelofdifficulty: "Strenuous"
      
     },
     {
+        imageurl: "",
         Mountname: "",
         description: "",
         elevation: "",
         location: "",
-        imageurl: "",
-        levelofdifficulty: ""
+        levelofdifficulty: "Strenuous"
      
     },
     {
+        imageurl: "",
         Mountname: "",
         description: "",
         elevation: "",
         location: "",
-        imageurl: "",
-        levelofdifficulty: ""
+        levelofdifficulty: "Strenuous"
      
     },
     {
+        imageurl: "",
         Mountname: "",
         description: "",
         elevation: "",
         location: "",
-        imageurl: "",
-        levelofdifficulty: ""
+        levelofdifficulty: "Strenuous"
      
     },
     {
+        imageurl: "",
         Mountname: "",
         description: "",
         elevation: "",
         location: "",
-        imageurl: "",
-        levelofdifficulty: ""
+        levelofdifficulty: "Strenuous"
      
     },
     {
+        imageurl: "",
         Mountname: "",
         description: "",
         elevation: "",
         location: "",
-        imageurl: "",
-        levelofdifficulty: ""
+        levelofdifficulty: "Strenuous"
      
     },
     {
+        imageurl: ""
         Mountname: "",
         description: "",
         elevation: "",
         location: "",
-        imageurl: "",
-        levelofdifficulty: ""
+        levelofdifficulty: "Strenuous"
      
     },
     {
+        imageurl: "",
         Mountname: "",
         description: "",
         elevation: "",
         location: "",
-        imageurl: "",
-        levelofdifficulty: ""
+        levelofdifficulty: "Strenuous"
      
     },
     {
+        imageurl: "",
         Mountname: "",
         description: "",
         elevation: "",
         location: "",
-        imageurl: "",
-        levelofdifficulty: ""
+       levelofdifficulty: "Strenuous"
      
     },
     {
+        imageurl: "",
         Mountname: "",
         description: "",
         elevation: "",
         location: "",
-        imageurl: "",
-        levelofdifficulty: ""
-     
-    },
-    {
-        Mountname: "",
-        description: "",
-        elevation: "",
-        location: "",
-        imageurl: "",
-        levelofdifficulty: ""
-     
-    },
-    {
-        Mountname: "",
-        description: "",
-        elevation: "",
-        location: "",
-        imageurl: "",
-        levelofdifficulty: ""
-     
-    },
-    {
-        Mountname: "",
-        description: "",
-        elevation: "",
-        location: "",
-        imageurl: "",
-        levelofdifficulty: ""
-     
-    },
-    {
-        Mountname: "",
-        description: "",
-        elevation: "",
-        location: "",
-        imageurl: "",
-        levelofdifficulty: ""
-     
-    },
-    {
-        Mountname: "",
-        description: "",
-        elevation: "",
-        location: "",
-        imageurl: "",
-        levelofdifficulty: ""
-     
-    },
-    {
-        Mountname: "",
-        description: "",
-        elevation: "",
-        location: "",
-        imageurl: "",
-        imageurl: "",
-        levelofdifficulty: ""
+        levelofdifficulty: "Strenuous"
       
       },
       {
+        imageurl: "",
+          Mountname: "",
+          description: "",
+          elevation: "",
+          location: "",
+          levelofdifficulty: "Strenuous"
+       
+      },
+      {
+        imageurl: "",
+          Mountname: "",
+          description: "",
+          elevation: "",
+          location: "",
+          levelofdifficulty: "Strenuous"
+       
+      },
+      {
+        imageurl: "",
+          Mountname: "",
+          description: "",
+          elevation: "",
+          location: "",
+          levelofdifficulty: "Strenuous"
+       
+      },
+      {
+        imageurl: "",
+          Mountname: "",
+          description: "",
+          elevation: "",
+          location: "",
+          levelofdifficulty: "Strenuous"
+       
+      },
+      {
+        imageurl: "",
+          Mountname: "",
+          description: "",
+          elevation: "",
+          location: "",
+          levelofdifficulty: "Strenuous"
+       
+      },
+      {
+        imageurl: "",
+          Mountname: "",
+          description: "",
+          elevation: "",
+          location: "",
+          levelofdifficulty: "Strenuous"
+       
+      },
+      {
+        imageurl: "",
           Mountname: "",
           description: "",
           elevation: "",
           location: "",
           imageurl: "",
-          levelofdifficulty: ""
+          levelofdifficulty: "Strenuous"
        
       },
       {
@@ -206,226 +260,172 @@
           description: "",
           elevation: "",
           location: "",
-          imageurl: "",
-          levelofdifficulty: ""
+          levelofdifficulty: "Strenuous"
        
       },
       {
+        imageurl: "",
           Mountname: "",
           description: "",
           elevation: "",
           location: "",
-          imageurl: "",
-          levelofdifficulty: ""
+          levelofdifficulty: "Strenuous"
        
       },
       {
+        imageurl: "",
           Mountname: "",
           description: "",
           elevation: "",
           location: "",
-          imageurl: "",
-          levelofdifficulty: ""
+          levelofdifficulty: "Strenuous"
        
       },
       {
+        imageurl: "",
           Mountname: "",
           description: "",
           elevation: "",
           location: "",
-          imageurl: "",
-          levelofdifficulty: ""
+          levelofdifficulty: "Strenuous"
        
       },
       {
+        imageurl: "",
           Mountname: "",
           description: "",
           elevation: "",
           location: "",
-          imageurl: "",
-          levelofdifficulty: ""
+          levelofdifficulty: "Strenuous"
        
       },
       {
+        imageurl: "",
           Mountname: "",
           description: "",
           elevation: "",
           location: "",
-          imageurl: "",
-          levelofdifficulty: ""
+          levelofdifficulty: "Strenuous"
        
       },
       {
+        imageurl: "",
           Mountname: "",
           description: "",
           elevation: "",
           location: "",
-          imageurl: "",
-          levelofdifficulty: ""
+          levelofdifficulty: "Strenuous"
        
       },
       {
+        imageurl: "",
           Mountname: "",
           description: "",
           elevation: "",
           location: "",
-          imageurl: "",
-          levelofdifficulty: ""
+          levelofdifficulty: "Strenuous"
        
       },
       {
+        imageurl: "",
           Mountname: "",
           description: "",
           elevation: "",
           location: "",
-          imageurl: "",
-          levelofdifficulty: ""
+          levelofdifficulty: "Strenuous"
        
       },
       {
+        imageurl: "",
           Mountname: "",
           description: "",
           elevation: "",
           location: "",
-          imageurl: "",
-          levelofdifficulty: ""
+          levelofdifficulty: "Strenuous"
        
       },
       {
+        imageurl: "",
           Mountname: "",
           description: "",
           elevation: "",
           location: "",
-          imageurl: "",
-          levelofdifficulty: ""
+          levelofdifficulty: "Strenuous"
        
       },
       {
+        imageurl: "",
           Mountname: "",
           description: "",
           elevation: "",
           location: "",
-          imageurl: "",
-          levelofdifficulty: ""
+          levelofdifficulty: "Strenuous"
        
       },
       {
-          Mountname: "",
-          description: "",
-          elevation: "",
-          location: "",
-          imageurl: "",
-          levelofdifficulty: ""
-       
-      },
-      {
-          Mountname: "",
-          description: "",
-          elevation: "",
-          location: "",
-          imageurl: "",
-          levelofdifficulty: ""
-       
-      },
-      {
-          Mountname: "",
-          description: "",
-          elevation: "",
-          location: "",
-          imageurl: "",
-          levelofdifficulty: ""
-       
-      },
-      {
-          Mountname: "",
-          description: "",
-          elevation: "",
-          location: "",
-          imageurl: "",
-          levelofdifficulty: ""
-       
-      },
-      {
-          Mountname: "",
-          description: "",
-          elevation: "",
-          location: "",
-          imageurl: "",
-          levelofdifficulty: ""
-       
-      },
-      {
-          Mountname: "",
-          description: "",
-          elevation: "",
-          location: "",
-          imageurl: "",
-          levelofdifficulty: ""
-       
-      },
-      {
+        imageurl: "",
         Mountname: "",
         description: "",
         elevation: "",
         location: "",
-        imageurl: "",
-        levelofdifficulty: ""
+        levelofdifficulty: "Strenuous"
      
     },  {
+        imageurl: "",
         Mountname: "",
         description: "",
         elevation: "",
         location: "",
-        imageurl: "",
-        levelofdifficulty: ""
+        levelofdifficulty: "Strenuous"
      
     },  {
+        imageurl: "",
         Mountname: "",
         description: "",
         elevation: "",
         location: "",
-        imageurl: "",
-        levelofdifficulty: ""
+        levelofdifficulty: "Strenuous"
      
     },  {
+        imageurl: "",
         Mountname: "",
         description: "",
         elevation: "",
         location: "",
-        imageurl: "",
-        levelofdifficulty: ""
+        levelofdifficulty: "Strenuous"
      
     },  {
+        imageurl: "",
         Mountname: "",
         description: "",
         elevation: "",
         location: "",
-        imageurl: "",
-        levelofdifficulty: ""
+        levelofdifficulty: "Strenuous"
      
     },  {
+        imageurl: "",
         Mountname: "",
         description: "",
         elevation: "",
         location: "",
-        imageurl: "",
-        levelofdifficulty: ""
+        levelofdifficulty: "Strenuous"
      
     },  {
+        imageurl: "",
         Mountname: "",
         description: "",
         elevation: "",
         location: "",
         imageurl: "",
-        levelofdifficulty: ""
+        levelofdifficulty: "Strenuous"
      
     },  {
+        imageurl: "",
         Mountname: "",
         description: "",
         elevation: "",
         location: "",
-        imageurl: "",
-        levelofdifficulty: ""
+        levelofdifficulty: "Strenuous"
      
     }];
  // Get the dropdown element and display the info
@@ -453,7 +453,7 @@ dropdown.addEventListener("change", function() {
             <h2>${selectedMountain.Mountname}</h2>
             <p><strong>Description:</strong> ${selectedMountain.description}</p>
             <p><strong>Elevation:</strong> ${selectedMountain.elevation}</p>
-            <p><strong>Location:</strong> ${selectedMountain.location}</p>
+            <p><strong>Location:</strong> ${selectedMountain.coords}</p>
             <p><strong>Level of Difficulty:</strong> ${selectedMountain.levelofdifficulty}</p>
             <img src="${selectedMountain.imageurl}" alt="${selectedMountain.Mountname}">
         `;
