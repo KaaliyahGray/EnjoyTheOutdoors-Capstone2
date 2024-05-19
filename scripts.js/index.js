dc0a68460dc713566713821125558b1d7fe364dd
KaaliyahGray <166437615+KaaliyahGray@users.noreply.github.com>
2024-05-18 21:12:09 -0700

    updated index
    updated index

--- scripts.js/index.js
+++ scripts.js/index.js
@@ -1,1 +1,11 @@
 "use strict";
+
+var swiper = new Swiper('.swiper-container', {
+    direction: 'vertical',
+    slidesPerView: 1,
+    mousewheel: true,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  });
